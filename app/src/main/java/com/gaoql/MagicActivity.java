e9d6293bb90af711769471cbd686d2eebc760a56
gaoqionglou <438279375@qq.com>
2017-06-12 23:49:33 +0800

    demo-magic页面

--- app/src/main/java/com/gaoql/MagicActivity.java
+++ app/src/main/java/com/gaoql/MagicActivity.java
@@ -1,83 +1,45 @@
 package com.gaoql;
 
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ImageView;
 
-import com.gaoql.customview.DividerGridItemDecoration;
+import com.gaoql.customview.CustomRelativeLauout;
 import com.gaoql.customview.RadianViewPager;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class MagicActivity extends AppCompatActivity {
-    RecyclerView recyclerView;
-    RadianViewPager radianViewPager;
-    private List<String> datas = new ArrayList<>();
-    RvAdapter rvAdapter =  new RvAdapter();
+public class MagicActivity extends AppCompatActivity implements RadianViewPager.OnPagerChangeListener{
+    private ImageView iv1,iv2,iv3,iv4;
+    private RadianViewPager rvp;
+    private CustomRelativeLauout crl;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_magic);
-        initData();
-        recyclerView = (RecyclerView)findViewById(R.id.rv);
-        radianViewPager =(RadianViewPager)findViewById(R.id.vp);
-        recyclerView.setLayoutManager(new GridLayoutManager(this,2));
-        recyclerView.setAdapter(rvAdapter);
-        radianViewPager.setOnPagerChangeListener(new RadianViewPager.OnPagerChangeListener() {
-            @Override
-            public void pageChanged(int from, int to) {
-                changeData(from,to);
-                rvAdapter.notifyItemRangeChanged(0,4);
-            }
-        });
+        crl = (CustomRelativeLauout) findViewById(R.id.crl);
+        rvp = (RadianViewPager)findViewById(R.id.vp);
+        iv1=(ImageView)findViewById(R.id.iv1);
+        iv2=(ImageView)findViewById(R.id.iv2);
+        iv3=(ImageView)findViewById(R.id.iv3);
+        iv4=(ImageView)findViewById(R.id.iv4);
+        rvp.setOnPagerChangeListener(this);
     }
 
-    private void initData(){
-        datas.add("春");
-        datas.add("夏");
-        datas.add("秋");
-        datas.add("冬");
+    @Override
+    public void pageChanged(int from, int to,float endx,float endy) {
+        startAnimation(iv1);
+        startAnimation(iv2);
+        startAnimation(iv3);
+        startAnimation(iv4);
+        crl.rotate();
     }
 
-    private void changeData(int from,int to){
-        datas.clear();
-        datas.add("春"+from+to);
-        datas.add("夏"+from+to);
-        datas.add("秋"+from+to);
-        datas.add("冬"+from+to);
-
-    }
-    class RvAdapter extends RecyclerView.Adapter<MagicActivity.MyViewHolder>{
-
-        @Override
-        public MagicActivity.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            MagicActivity.MyViewHolder myViewHolder = new MagicActivity.MyViewHolder(LayoutInflater.from(MagicActivity.this).inflate(R.layout.layout_content_item,parent,false));
-            return myViewHolder;
-        }
-
-        @Override
-        public void onBindViewHolder(MagicActivity.MyViewHolder holder, int position) {
-
-            holder.textView.setText(datas.get(position));
-        }
-
-        @Override
-        public int getItemCount() {
-            return datas.size();
-        }
-    }
-
-    class MyViewHolder extends RecyclerView.ViewHolder{
-        TextView textView;
-        public MyViewHolder(View itemView) {
-            super(itemView);
-            textView= (TextView) itemView.findViewById(R.id.content_text);
-        }
+    public void startAnimation(View v){
+        PropertyValuesHolder pvhX = PropertyValuesHolder.ofFloat("alpha", 1f, 0f, 1f);
+        PropertyValuesHolder pvhY = PropertyValuesHolder.ofFloat("scaleX", 1f, 0f, 1f);
+        PropertyValuesHolder pvhZ = PropertyValuesHolder.ofFloat("scaleY", 1f, 0f, 1f);
+        ObjectAnimator.ofPropertyValuesHolder(v, pvhX, pvhY,pvhZ).setDuration(1000).start();
     }
 }
